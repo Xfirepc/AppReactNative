9473594f1028ca5b43497506ed5d78b76008b5f4
xfirepc <vinygfx@gmail.com>
2019-03-17 01:36:47 -0500

    add styles to flex suggestion

--- src/videos/components/suggestion.js
+++ src/videos/components/suggestion.js
@@ -5,11 +5,17 @@ function Suggestion ( props ){
   return(
     <View style = { styles.container } >
       
-      <View style={ styles.left}>
+      <View style={ styles.left }>
         <Image style={ styles.cover } source={require('../../../assets/logo.png')}/>
+        <View style={ styles.gener }>
+          <Text style={ styles.generText }>Accion</Text>
+        </View>
       </View>
       
-      <View style={styles.right}>
+      <View style={ styles.right }>
+        <Text style={ styles.title }>Avengers</Text>
+        <Text style={ styles.year }>2012</Text>
+        <Text style={ styles.rating }>5 Estrellas</Text>
       </View>
 
     </View>
@@ -17,8 +23,48 @@ function Suggestion ( props ){
 }
 
 const styles = StyleSheet.create({
-  separator:{
-    borderTopWidth: 1,
+  container:{
+    flexDirection: 'row',
+  },
+  gener:{
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    backgroundColor: 'black',
+    paddingVertical: 5,
+    paddingHorizontal: 7,
+  },
+  generText: {
+    color: 'white',
+    fontSize: 11,
+  },
+  cover: {
+    height: 100,
+    width: 150,
+    resizeMode: 'contain'
+  },
+  right: {
+    paddingLeft: 10,
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 18,
+    color: '#44546b',
+  },
+  year: {
+    backgroundColor: '#70b124',
+    paddingVertical: 4,
+    color: '#fff',
+    fontSize: 11,
+    borderRadius: 5,
+    overflow: 'hidden',
+    paddingHorizontal: 6,
+    alignSelf: 'flex-start',
+  },
+  rating: {
+    color: '#6b6b6b',
+    fontSize: 14,
+    fontWeight: 'bold',
   }
 })
 export default Suggestion
